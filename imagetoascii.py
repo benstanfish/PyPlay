a60c424a2f1aa5a9ca46bbd9c28a47a8649e80fe
benstanfish <benstanfish@hotmail.com>
2023-08-02 08:41:14 -0700

    Initial commit

---
+++ imagetoascii.py
@@ -0,0 +1,10 @@
+
+
+
+import numpy as np
+
+SCALE70 = '$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,\"^`". '
+SCALE10 = '@%#*+=-:. '
+
+scale70 = np.asarray(list(SCALE70))
+scale10 = np.asarray(list(SCALE10))
